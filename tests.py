52ad96ce78a674597ae9518bc9d99298c5e37eb7
pranavr2003 <rsaipranav2003@gmail.com>
2021-09-18 13:26:22 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
