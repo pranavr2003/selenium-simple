1456f5d94775624a5d5ba26da6b3a7bf820a0b83
pranavr2003 <rsaipranav2003@gmail.com>
2021-10-08 18:25:09 +0530

    main code to be executed, given a url
    another file will be created that imports this and runs a single line, if not a pypi package

---
+++ src/main_to_ex.py
@@ -0,0 +1,14 @@
+from use_regex_to_filter_url import filter_stuff
+from check_if_valid_url import check_if_valid_url
+from char_to_py_code import char_to_py_code
+
+def main_to_ex(url):
+    filter_stuff(url)
+    check_if_valid_url(url)
+    char_to_py_code(url)
+
+    # for item in filter_stuff(url):
+    #     print(item)
+
+main_to_ex(r"sel-simple/|https://www.python.org|/^print('yee')^/?febn=q/c/k-pycon?K-RETURN/}")
+
