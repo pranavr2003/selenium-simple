18887b334ad630470cdae3e9106910bbdcc9926a
pranavr2003 <rsaipranav2003@gmail.com>
2021-10-08 09:56:06 +0530

    returns spilt route, url_to_use

--- src/check_if_valid_url.py
+++ src/check_if_valid_url.py
@@ -1,7 +1,7 @@
 from use_regex_to_filter_url import filter_stuff
 import warnings
 
-ex_str = r'sel-simple/|https://www.python.org/|/^assert "Python" in title^/?febn=q/c/k-pycon?K-RETURN/}'
+# ex_str = r'sel-simple/|https://www.python.org/|/^assert "Python" in title^/?febn=q/c/k-pycon?K-RETURN/}'
 
 def check_if_valid_url(url):
     """Checks if the URL is in the correct format
@@ -19,6 +19,8 @@ def check_if_valid_url(url):
         print(warnings.WarningMessage("The route must start with 'sel-simple/|URL|...'", UserWarning, 'check_if_valid_url.py', 12))
 
     return [route.split('/'), url_to_use]
+
+# print(check_if_valid_url(ex_str))
 
 # print(check_if_valid_url(ex_str))
 
