3517bec9201b6c676c3080d11d0c4b22d7b9dbc1
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-10-12 12:17:34 +0530

    Update char_to_py_code.py

--- src/char_to_py_code.py
+++ src/char_to_py_code.py
@@ -2,25 +2,23 @@ from check_if_valid_url import *
 from selenium import webdriver
 
 def str_to_py_code(get_input_str:str):
-    """Converts the given string starting with `^` to Python code \n
-    Every char more than one `^` counts one indent up
-    """
+    """Converts the given string starting with `^` to Python code.
+    Every char more than one `^` counts one indent up."""
+
     if get_input_str.startswith('^'):
         count = get_input_str.count('^')
         # print(count)
         count_divided = count/2
         keep_indent_count = -1
         check_indent_count = int(keep_indent_count + count_divided)
-
         code_to_exec = '\n' + '    '*check_indent_count + get_input_str.replace('^', '').replace('\\', '/')
 
         return code_to_exec
 
 
 def char_to_py_code(url):
-    """Converts char `^` to Python code \n
-    Every char more than one `^` counts one indent up
-    """
+    """Converts char `^` to Python code.
+    Every char more than one `^` counts one indent up."""
     check_for_validity = check_if_valid_url(url)
     get_split_list = check_for_validity[0]
 
