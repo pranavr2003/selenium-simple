e8c9b8649c134e63bb8b5e0731280d2f387dad9b
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-10-12 12:18:00 +0530

    Update main_to_ex.py

--- src/main_to_ex.py
+++ src/main_to_ex.py
@@ -4,12 +4,9 @@ from char_to_py_code import char_to_py_code
 from add_options_def_driver import *
 
 def main_to_ex(url):
-
     """Executes the mentioned URL"""
-
     filter_stuff(url)
     check_if_valid_url(url)
     add_arg_options(url)
-
 
 main_to_ex(r'sel-simple/|https://python.org|/#user-data-dir=C:\Users\SONY\AppData\Local\Google\Chrome\User Data/#profile-directory=Profile 1/^assert "fuck" in driver.title^/?febn=q/c/k-pycon?K-RETURN/}')
